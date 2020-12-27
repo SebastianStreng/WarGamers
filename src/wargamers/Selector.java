0fc26d589a500572c2e1f5e03dd48701f06082cd
SebastianStreng <51957686+SebastianStreng@users.noreply.github.com>
2020-12-27 18:37:13 +0100

    deleted king-seperated-classes
    deleted speacial-attack-kings --> give each king per parameter

--- src/wargamers/Selector.java
+++ src/wargamers/Selector.java
@@ -13,22 +13,23 @@ import wargamers.Kings;
  */
 public class Selector {
     
-    public void selectInteraction(){
+    public void selectInteraction(Kings king1, Kings king2, int damageStrength){
         
-        System.out.println("Please select one of the following interations: ");
+        System.out.println("Please select one of the following interactions: ");
         
         System.out.println("1. attack");
         System.out.println("2. defense");
+        System.out.println("3. run over");
+        System.out.println("4. Special attack");
         
         Scanner myScanner = new Scanner(System.in);
         int decision = myScanner.nextInt();
         
-        
-        
-        
-        
         switch (decision){
-        //case 1: Kings.attack(king1, king2, decision);
+            case 1:
+                wargamers.Kings.attack(king1, king2, damageStrength);
+        }
+        
     }
         
         
